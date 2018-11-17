b19243be5855086c72caf613f6beb233d47b6bcd
LFH <lfh@fenhua.local>
2018-11-17 14:23:29 +0800

    add strutil folder

--- 02_packages/main.go
+++ 02_packages/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"math"
+	"02_packages/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.3))
 	fmt.Println(math.Ceil(2.4))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(strutil.Reverse("olleh"))
 }
