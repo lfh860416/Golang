692ae4e538b0507a67d119a3cf439378ce166e9c
LFH <lfh@fenhua.local>
2018-11-28 23:12:07 +0800

    go by example - switch

--- gobyexample/main.go
+++ gobyexample/main.go
@@ -80,16 +80,24 @@ func main() {
 	t := time.Now()
 	switch {
 	case t.Hour() < 12:
-		fmt.Println("it's before noon")
+		fmt.Println("It's before noon")
 	default:
-		fmt.Println("it's after noon")
+		fmt.Println("It's after noon")
 	}
 
-
-
-
-
-
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Unknown type %T\n", t)
+		}	
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 
 	
 }
