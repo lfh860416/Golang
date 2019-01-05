fc8629a3d3cf017e1adc53e59fc12648104a8f3d
LFH <lfh@fenhua.local>
2019-01-05 16:08:15 +0800

    add maps range pointers

--- 07_maps/main.go
+++ 07_maps/main.go
@@ -1,3 +1,23 @@
-package func main() {
-	
+package main
+
+import "fmt"
+
+func main() {
+	//define map
+	emails := make(map[string]string)
+	//assign kv
+	emails["Bob"] = "bob@gmail.com"
+	emails["Mike"] = "mike@gmail.com"
+
+	//declare map and add kv
+	emails2 := map[string]string{"Alex":"alex@gmail.com", "Winnie":"winnie@gmail.com"}
+
+	fmt.Println(emails)
+	fmt.Println(len(emails))
+	fmt.Println(emails["Mike"])
+	fmt.Println("emails2 is", emails2)
+
+	//delete from map
+	delete(emails2, "Alex")
+	fmt.Println("emails2 is", emails2)
 }
